eaf9eb02aa322113bb4b7f124d882e0423c11b06
valentincoletta322 <81708912+valentincoletta322@users.noreply.github.com>
2022-04-20 11:00:43 -0300

    Add files via upload

---
+++ tp10.2/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args){
+        ArrayList<Integer> listaDeNumeros = new ArrayList<>();
+
+        System.out.println("Ingresar la cantidad de numeros");
+
+        Scanner ingreso1 = new Scanner(System.in);
+        int cantNumeros = ingreso1.nextInt();
+
+        for(int i = 0; i<cantNumeros; i++){
+            Scanner ingreso2 = new Scanner(System.in);
+            listaDeNumeros.add(ingreso2.nextInt());
+        }
+
+        HashSet<Integer> hashSetNumeros = new HashSet<>();
+
+        for (int num:listaDeNumeros) {
+            hashSetNumeros.add(num);
+        }
+
+    }
+
+
+
+}
